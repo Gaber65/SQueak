874c7f51c48e85a793bb981321c7382fe9c7045c
Mahmoud Said <102619410+mahmoudsaid109@users.noreply.github.com>
2025-10-13 12:02:26 +0300

    use kDebugMode and debugPrint in bloc observer for improve performance

--- lib/core/service/main_service/presentation/widgets/init_functions.dart
+++ lib/core/service/main_service/presentation/widgets/init_functions.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:squeak/core/utils/export_path/export_files.dart';
 import 'package:squeak/core/utils/firebase_token_helper.dart';
 import 'package:squeak/features/layout/notification/NotificationFCM/notification_message.dart';
@@ -15,6 +16,7 @@ class InitFunctions {
   static Future<void> initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
     ConfigModel.setEnvironment(Environment.test);
+        Bloc.observer = MyBlocObserver();
     await _initServiceLocator();
     await _initCache(); // Initialize cache first
     await NotificationInitializer.initialize();
---
+++ lib/core/service/observer/observer.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: avoid_print
+
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    if (kDebugMode) {
+      debugPrint('DEBUG: onCreate -- ${bloc.runtimeType}');
+    }
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    if (kDebugMode) {
+      debugPrint('DEBUG: onChange -- ${bloc.runtimeType}, $change');
+    }
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    if (kDebugMode) {
+      debugPrint('DEBUG: onError -- ${bloc.runtimeType}, $error');
+    }
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    if (kDebugMode) {
+      debugPrint('DEBUG: onClose -- ${bloc.runtimeType}');
+    }
+  }
+}
