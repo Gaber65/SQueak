a9970318c023a6a4cebf8f0151c56595d666b442
Gaber28 <mamdohali72@gmail.com>
2025-12-24 16:20:38 +0200

    facebook login

--- lib/features/layout/layout/presentation/screens/layout_screen.dart
+++ lib/features/layout/layout/presentation/screens/layout_screen.dart
@@ -195,7 +195,7 @@ class _LayoutScreenState extends State<LayoutScreen> with SingleTickerProviderSt
             extendBody: false,
             resizeToAvoidBottomInset: false,
 
-            body: cubit.screens[selectedIndex],
+            body: cubit.screensPets[selectedIndex],
 
             bottomNavigationBar: BlocConsumer<MainCubit, MainState>(
               listener: (context, state) {},
